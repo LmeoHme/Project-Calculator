aa63e934964472ee490a0bf753780052bc9e381e
LmeoHme <luatnguyengia00@gmail.com>
2026-05-08 17:23:52 +0900

    feat: Add invalid input handler.

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
- {
+const calculatorScreen = document.querySelector("#calculator-screen");
+const outputBox = calculatorScreen.firstElementChild;
+const inputBox = calculatorScreen.lastElementChild;
 
+const buttons = document.querySelector("#buttons");
+
+function invalidInputHandler()
+{
+    const EMPTY_STRING = "";
+    const VALID_MULTIPLY = "x";
+
+    const invalidInput = /[^0-9.+\-\*x\/]/g;
+    const invalidMultiply = /\*/g;
+
+    return () => {
+        inputBox.value = inputBox.value
+                            .replace(invalidInput, EMPTY_STRING)
+                            .replace(invalidMultiply, VALID_MULTIPLY);
+    };
 }
+
+const invalidInputHandling = invalidInputHandler();
+
+inputBox.addEventListener("input", e => {
+    e.preventDefault();
+    invalidInputHandling();
+});
+
+buttons.addEventListener("click", e => {
+    if (e.target && e.target.nodeName === "LI")
+    {
+        inputBox.value += e.target.innerText;
+    }
+});
+
+
+
+// Align operator to center vertically needs adding a span element with display: inline-flex, will be 
+// applied when implement EnteringOperator state
